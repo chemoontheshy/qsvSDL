8733064effbc0abf0fcde4eee2f764b03e84a32b
chemoontheshy <xzfandzgx@gmail.com>
2021-06-08 13:53:26 +0800

    添加项目文件。

---
+++ src/qsvSDL/main.cpp
@@ -0,0 +1,23 @@
+//当前C++兼容C语言
+extern "C"
+{
+	//avcodec:编解码(最重要的库)
+#include <libavcodec/avcodec.h>
+//avformat:封装格式处理
+#include <libavformat/avformat.h>
+//swscale:视频像素数据格式转换
+#include <libswscale/swscale.h>
+//avdevice:各种设备的输入输出
+#include <libavdevice/avdevice.h>
+//avutil:工具库（大部分库都需要这个库的支持）
+#include <libavutil/avutil.h>
+}
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << avcodec_configuration() << endl;
+}
