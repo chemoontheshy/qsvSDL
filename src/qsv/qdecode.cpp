e1162ef1d54c15041700c258f2a273159f9a6336
chemoontheshy <xzfandzgx@gmail.com>
2021-06-23 16:10:14 +0800

    删去网络设置

--- src/qsv/qdecode.cpp
+++ src/qsv/qdecode.cpp
@@ -52,22 +52,12 @@ void QDecode::setUrl(const char* _url)
 /// </summary>
 void QDecode::play()
 {
-	//在打开码流前指定各种参数比如:探测时间/超时时间/最大延时等
-   //设置缓存大小,1080p可将值调大
-	av_dict_set(&avdic, "buffer_size", "8192000", 0);
-	//以tcp方式打开,如果以udp方式打开将tcp替换为udp
-	av_dict_set(&avdic, "rtsp_transport", "udp", 0);
-	//设置超时断开连接时间,单位微秒,3000000表示3秒
-	av_dict_set(&avdic, "stimeout", "3000000", 0);
-	//设置最大时延,单位微秒,1000000表示1秒
-	av_dict_set(&avdic, "max_delay", "1000000", 0);
-	//自动开启线程数
-	av_dict_set(&avdic, "threads", "auto", 0);
+	
 
 	pFormatCtx = avformat_alloc_context();
 
 	//打开视频流
-	int ret = avformat_open_input(&pFormatCtx, url, nullptr, &avdic);
+	int ret = avformat_open_input(&pFormatCtx, url, nullptr, nullptr);
 	if (ret < 0) {
 		cout << "open url error" << endl;
 		return;
